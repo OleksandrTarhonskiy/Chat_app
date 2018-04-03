18d00dca0736013902649e1eb72a09fd38c29a8b
alextargonski <targonsky97@gmail.com>
2018-04-03 17:04:34 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   mount ActionCable.server => '/cable'
   resources :messages
   resource :sessions, only: :create
-  resources :users, only: [:create]
+  resources :users
 end
